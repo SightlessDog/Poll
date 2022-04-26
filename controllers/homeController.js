882a1fb3633f5698775d608db2086ba14c6b9f43
s0572928 <martin.staats@student.htw-berlin.de>
2022-04-26 21:03:05 +0200

    add route and parameter for map pinning

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,3 +2,11 @@ exports.sendReqParam = (req, res) => {
     let veg = req.params.vegetable;
     res.send(`This is the page for ${veg}`);
 };
+
+exports.sendCoordinates = (req, res) => {
+    let lat = req.params.lat;
+    let lon = req.params.lon;
+    // Set id for the pin
+    // let id = id.set(); 
+    res.send("Coordinates for the Pin are:" + lat + ", " + lon);
+};
