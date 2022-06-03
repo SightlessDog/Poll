d21be151e1b99582bdaf314d35ea1ab07af1fb67
Your Name <ptsvyatkov97@gmail.com>
2022-06-03 17:15:05 +0200

    Feature 009 - Added password hashing with bcrypt, user login/authentication

--- routes/user.js
+++ routes/user.js
@@ -4,12 +4,12 @@ const router = require("express").Router(),
     userController = require("../controllers/userController"),
     secondUserController = require("../controllers/secondUserController");
 
-router.post("/", userController.validate, userController.createUser, userController.redirectView);
+router.post("/register", userController.validate, userController.createUser, userController.redirectView);
 router.get("/", secondUserController.index);
 router.get("/register", userController.showRegister);
 router.get("/profile", userController.showProfile);
 router.get("/signIn", userController.showSignInPage);
 router.get("/resetPassword", userController.showResetPassword);
 router.get("/forgotPassword", userController.showForgotPassword);
-
+router.post("/signIn", userController.validatePasswordHash, userController.authenticate);
 module.exports = router;
