8387f16ee6b48b62593cdb274135a6d479ab24bf
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-04 15:03:40 +0200

    remove second user

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const event = require("./models/event");
 const router = require("./routes/router");
 const passport = require("passport");
 const User = require("./models/user"); //needed functionality for passport to work
-const SecondUser = require("./models/second-user");
 const expressSession = require("express-session");
 app.use(
     expressSession({
