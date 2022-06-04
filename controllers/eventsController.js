c5530b57832b68f03932f0df7cfca7b132d14c4f
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-04 14:19:10 +0200

    Remove closedPoll references

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose');
 const Event = require("../models/event");
-const ClosedPoll = require("../models/closedPoll");
 
 let events;
-let closedPolls;
 
 Event.find({}, (err, res) => events = res);
-
-ClosedPoll.find({}, (err, res) => closedPolls = res);
 
 module.exports = {
     showEvents : (req, res) => {
@@ -24,19 +20,5 @@ module.exports = {
                 console.log("Events: Promise complete");
             });
 
-    },
-    showClosedPolls : (req, res) => {
-        ClosedPoll.find({}).exec()           //return promise from find query
-            .then((closedPolls) =>{          //send data to next codeblock
-                res.render("Events/events", {  
-                    closedPolls: closedPolls
-                });                     //serve results from db
-            }).catch((error) => {
-                console.log(error.message);
-                return [];
-            })                          //catch rejected errors that are rejected in promise
-            .then(() => {
-                console.log("Voted Poll: Promise complete");
-            });
     }
 }
