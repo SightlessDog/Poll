917e41106ae9db9cd1768b7c9ff272ed120c20a1
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-06-16 17:13:48 +0200

    Revert "feat(Hashing): add hashing and validating according to the book"
    This reverts commit ce0139dd0a8364d0dd6e224f897c26d2eee3a07c.

--- models/user.js
+++ models/user.js
@@ -1,60 +1,38 @@
-const mongoose = require('mongoose'),
-  { Schema } = require('mongoose'),
-  passportLocalMongoose = require('passport-local-mongoose');
-
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose"),
+    { Schema } = require("mongoose"),
+    passportLocalMongoose = require("passport-local-mongoose")
 
 const userSchema = new Schema({
-  // name:{
-  //     first:{
-  //         type: String,
-  //         trim:true,
-  //         required: true           // will be needed later
-  //     },
-  //     last:{
-  //         type: String,
-  //         trim:true,
-  //         required: true
-  //     }
-  // },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+    // name:{
+    //     first:{
+    //         type: String,
+    //         trim:true,
+    //         required: true           // will be needed later
+    //     },
+    //     last:{
+    //         type: String,
+    //         trim:true,
+    //         required: true
+    //     }
+    // },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    }  
 });
 
-userSchema.virtual('fullName').get(function () {
-  return `${this.name.first} ${this.name.last}`;
+userSchema.virtual("fullName").get(function() {
+    return `${this.name.first} ${this.name.last}`;
 });
 
 userSchema.plugin(passportLocalMongoose, {
-  usernameField: 'email',
+    usernameField: "email"
 });
 
-userSchema.pre('save', function (next) {
-  let user = this;
-
-  bcrypt
-    .hash(user.password, 10)
-    .then((hash) => {
-      user.password = hash;
-      next();
-    })
-    .catch((error) => {
-      console.log(`Error in hashing password: ${error.message}`);
-      next(error);
-    });
-});
-
-userSchema.methods.passwordComparison = function (inputPassword) {
-  let user = this;
-  return bcrypt.compare(inputPassword, user.password);
-};
-
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ router.post("/register", userController.validate, userController.createUser, use
 router.get("/register", userController.showRegister);
 router.get("/profile", userController.showProfile);
 router.get("/signIn", userController.showSignInPage);
-router.post("/signIn", userController.validatePasswordHash, userController.authenticate);
 router.get("/resetPassword", userController.showResetPassword);
 router.get("/forgotPassword", userController.showForgotPassword);
+router.post("/signIn", userController.validatePasswordHash, userController.authenticate);
 module.exports = router;
