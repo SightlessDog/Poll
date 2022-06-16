868e60b91dde1471b209120e6d7df30a45c43469
Elyess Eleuch <ely.369@outlook.com>
2022-06-16 19:38:09 +0200

    feat(Registration): add registration

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ const { body, validationResult } = require('express-validator');
 
 const getUserInfo = (body) => {
   return {
+    name: body.name,
     email: body.email,
     password: body.password,
   };
@@ -57,7 +58,16 @@ module.exports = {
     newUser.password = await bcrypt.hash(newUser.password, salt);
 
     //register the user and save in the database or throw an error if unsuccessful
-    User.register(newUser, req.body.password, (user, error) => {
+    User.create({name: newUser.name, email: newUser.email, password: newUser.password}, (error, user) => {
+      if (error) {
+        res.locals.redirect = '/Register/index';
+        req.flash(
+          'error',
+          `Failed to create user account because: ${error.message}.`
+        );
+        console.log('Unsuccessful registration!');
+        next();
+      }
       if (user) {
         req.flash(
           'success',
@@ -67,15 +77,7 @@ module.exports = {
         res.locals.redirect = '/';
         console.log('Successfully registered!');
         next();
-      } else {
-        res.locals.redirect = '/Register/index';
-        req.flash(
-          'error',
-          `Failed to create user account because: ${error.message}.`
-        );
-        console.log('Unsuccessful registration!');
-        next();
-      }
+      } 
     });
   },
 
