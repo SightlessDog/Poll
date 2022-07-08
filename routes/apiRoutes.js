bfb154ede13b3e903a52991abe247e2fcba021a0
Your Name <ptsvyatkov97@gmail.com>
2022-07-08 14:59:50 +0200

    removed api/allPolls and will be added later in different js file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,6 @@ const router = require("express").Router(),
 
 router.post("/polls", userController.apiAuthenticate);
 router.get("/polls", apiController.filterUserPolls, apiController.respondJSON);
-router.get("/allPolls", apiController.showPollsResponseJSON);
 router.use(userController.verifyJWT);
 router.use(apiController.errorJSON);
 
