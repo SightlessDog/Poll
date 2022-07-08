77172f56e7fb00578f12e764f5b1e1cc5a2aaaef
Your Name <ptsvyatkov97@gmail.com>
2022-07-08 15:27:31 +0200

    added another public js file to make ajax call for all polls over the api

--- public/js/allPolls.js
+++ public/js/allPolls.js
@@ -1,20 +1,17 @@
 $(document).ready(() => {
-    $("#modal-button").click(() => {
-        $(".modal-body").html('');
-        $.get("/api/polls", (data) => {
+        $.get("/api/allPolls", (data) => {
             if (!data) {
                 return;
             }
             console.log("Data is: ", data);
-                data.forEach((poll) => {
-                        $(".modal-body").append(
-                            `<div>
+            data.forEach((poll) => {
+                $(".modal-body").append(
+                    `<div>
                         <span class="polls-title">  Title: ${poll.title} </span>
                         <div class="polls-description"> Description: ${poll.description} </div>                 
                         </div>`
-                        );
+                );
 
-                });
+            });
         });
-    });
 });
